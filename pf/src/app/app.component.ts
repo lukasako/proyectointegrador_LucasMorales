05076759178bd2b30e1d73cff13c69282a22d9ca
Lukasako <lucasmoralesar@gmail.com>
2023-05-01 19:33:34 -0300

    se agrega experiencia laboral y educacion

--- pf/src/app/app.component.ts
+++ pf/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-header',
+  templateUrl: '../../src/app/components/header/header.component.html',
+  styleUrls: ['../../src/app/components/header/header.component.css']
 })
 export class AppComponent {
   title = 'pf';
--- pf/src/app/app.module.ts
+++ pf/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
+import { BodyComponent } from './components/body/body.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    BodyComponent,
+    AboutComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
