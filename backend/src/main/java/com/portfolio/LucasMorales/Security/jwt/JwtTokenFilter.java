e20a0736a0c2722ccecb5a54dd69ef83cf95a1d3
Lukasakoo <lukasmoralesarg@gmail.com>
2023-05-12 10:58:34 -0300

    Se agrega el package jwt dentro de security

---
+++ backend/src/main/java/com/portfolio/LucasMorales/Security/jwt/JwtTokenFilter.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.LucasMorales.Security.jwt;
+
+import com.portfolio.LucasMorales.Security.Service.UserDetailsImpl;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ *
+ * @author Lucas Morales
+ */
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+
+    @Autowired
+    JwtProvider jwtProvider;
+    @Autowired
+    UserDetailsImpl userDetailsImpl;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = getToken(request);
+            if (token != null && jwtProvider.validateToken(token)) {
+                String userName = jwtProvider.getNombreUsuarioFromToken(token);
+                UserDetails userDetails = userDetailsImpl.loadUserByUsername(userName);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (Exception e) {
+            logger.error("method doFilterInternal failed");
+        }
+        filterChain.doFilter(request, response);
+    }
+    private String getToken(HttpServletRequest request){
+        String header= request.getHeader("Authorization");
+        if(header != null && header.startsWith("Bearer"))
+            return header.replace("Bearer", "");
+        return null;
+    } 
+}
